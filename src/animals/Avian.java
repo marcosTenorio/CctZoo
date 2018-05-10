4617d0a62cca4a55e3c1d4c60d6f09fe7ea2d3b4
Gabriel Oliveira <ras.vibpositive@gmail.com>
2018-05-10 10:00:39 +0100

    Finishes javadoc for Avian

--- src/animals/Avian.java
+++ src/animals/Avian.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package animals;
 
 import java.util.Random;
@@ -19,57 +14,56 @@ public class Avian extends Animal{
     
     Random r = new Random();
     
-    /**
-     *
-     */
     public StoredDataUtility sDU = new StoredDataUtility();
     
     /**
-     *
+     * Initializes a newly created Avian object so that it represents an empty Animal.
      */
     public Avian(){
-        this.setFeathered(r.nextBoolean());
-        if(this.isFeathered() != false){
-            this.setFeathersColor(sDU.color[r.nextInt(sDU.color.length)]);
+        setFeathered(r.nextBoolean());
+        if(isFeathered() != false){
+            setFeathersColor(sDU.color[r.nextInt(sDU.color.length)]);
         }
     }
     
     /**
-     *
-     * @return
+     * Returns the colour of the animal's feathers
+     * @return A String with the colours of the feathers of the Avian
      */
     public String getFeathersColor() {
         return feathersColor;
     }
 
     /**
-     *
-     * @param feathersColor
+     * Sets the colours of the feathers
+     * @param feathersColor A String containing the colours of the feathers
      */
-    public void setFeathersColor(String feathersColor) {
+    public final void setFeathersColor(String feathersColor) {
         this.feathersColor = feathersColor;
     }
 
     /**
-     *
-     * @return
+     * Returns a boolean whether the Avian is feathered
+     * @return a boolean true if animal is feathered, false if it is not
      */
-    public boolean isFeathered() {
+    public final boolean isFeathered() {
         return feathered;
         
     }
 
     /**
-     *
-     * @param feathered
+     * Sets a boolean to define if animal is feathered
+     * @param feathered A boolean to define if Animal has feathers
      */
-    public void setFeathered(boolean feathered) {
+    public final void setFeathered(boolean feathered) {
         this.feathered = feathered;
     }
     
     /**
-     *
-     * @return
+     * Returns unique properties of class
+     * Method is invoked on superclass
+     * @return A String showing the values of feathered and feathersColor
+     * @see Animal#toString() 
      */
     @Override
     public String getProperties(){
